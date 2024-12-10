f46cdd5e590e2dc91133f2f4a8d5ffe7a6fdc194
Satyam-Nayak <satyamn120@gmail.com>
2024-12-11 01:22:36 +0530

    add css to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,14 @@ function App() {
     <div className="App">
       {!loggedIn ? (
         <div className="logIn">
+          <div className='inputs'>
           <input type='text' placeholder='Name.....'/>
           <input type='text' placeholder='Room.....'/>
+          </div>
           <button>Enter Chat</button>
         </div>
-      ) : <h1>You are Logged In</h1> }
+      ) : (<h1>You are Logged In</h1> 
+      )}
     </div>
   );
 }
