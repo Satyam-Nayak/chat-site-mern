6d6437c7b10a54ccd79bab8d56e577540b13ac16
Satyam-Nayak <satyamn120@gmail.com>
2024-12-13 01:36:56 +0530

    work on backend socket.io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,27 @@
 const express = require ('express')
+const socket = require('socket.io')
 const app = express()
-const cors = require('cors')
+const cors = require('cors');
 
-app.use(cors())
 
-app.listen('3002',()=>{
+app.use(cors());
+app.use(express.json()) // this allows us to send information from frontend to backend
+
+
+const server = app.listen('3002',()=>{
     console.log("server is running on port 3002")
+})
+
+io = socket(server)
+
+io.on('connection', (socket) => {
+    console.log(socket.id)
+
+    socket.on('join_room', (data)=>{
+        socket.join(data)
+    })
+
+    socket.on('disconnect',()=>{
+        console.log('User Disconnect..')
+    })
 })
